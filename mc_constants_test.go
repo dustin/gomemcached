32ee97dfae935cdb36a2fac418b010d59b949ecb
Dustin Sallings <dustin@spy.net>
2012-12-08 17:27:57 -0800

    TapConnectFlag type and stringing.

--- mc_constants_test.go
+++ mc_constants_test.go
@@ -26,3 +26,25 @@ func TestStatusNameString(t *testing.T) {
 		t.Fatalf("Expected \"0x80\" for 0x80, got \"%v\"", s.String())
 	}
 }
+
+func TestTapConnectFlagNameString(t *testing.T) {
+	if DUMP.String() != "DUMP" {
+		t.Fatalf("Expected \"DUMP\" for DUMP, got \"%v\"",
+			DUMP.String())
+	}
+
+	f := TapConnectFlag(0x3)
+	exp := "BACKFILL|DUMP"
+	if f.String() != exp {
+		t.Fatalf("Expected %q for 0x3, got %q", exp, f.String())
+	}
+
+	f = TapConnectFlag(0x212)
+	exp = "DUMP|SUPPORT_ACK|0x200"
+	if f.String() != exp {
+		t.Fatalf("Expected %q for 0x212, got %q", exp, f.String())
+	}
+
+	f = TapConnectFlag(0xffffffff)
+	f.String() // would hang if I were stupid
+}
