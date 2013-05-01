1aff90a62a4231a1563b327b00209ad8221589ed
Dustin Sallings <dustin@spy.net>
2013-04-30 23:01:59 -0700

    Increase encoding request and response coverage.

--- mc_req_test.go
+++ mc_req_test.go
@@ -1,6 +1,7 @@
 package gomemcached
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
@@ -28,6 +29,84 @@ func TestEncodingRequest(t *testing.T) {
 		0x0, 0x0, 0x0, 0x0, 0x37, 0xef, 0x3a, 0x35, // CAS
 		's', 'o', 'm', 'e', 'k', 'e', 'y',
 		's', 'o', 'm', 'e', 'v', 'a', 'l', 'u', 'e'}
+
+	if len(got) != req.Size() {
+		t.Fatalf("Expected %v bytes, got %v", got,
+			len(got))
+	}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Fatalf("Expected:\n%#v\n  -- got -- \n%#v",
+			expected, got)
+	}
+}
+
+func TestEncodingRequestWithExtras(t *testing.T) {
+	req := MCRequest{
+		Opcode:  SET,
+		Cas:     938424885,
+		Opaque:  7242,
+		VBucket: 824,
+		Extras:  []byte{1, 2, 3, 4},
+		Key:     []byte("somekey"),
+		Body:    []byte("somevalue"),
+	}
+
+	buf := &bytes.Buffer{}
+	req.Transmit(buf)
+	got := buf.Bytes()
+
+	expected := []byte{
+		REQ_MAGIC, byte(SET),
+		0x0, 0x7, // length of key
+		0x4,       // extra length
+		0x0,       // reserved
+		0x3, 0x38, // vbucket
+		0x0, 0x0, 0x0, 0x14, // Length of remainder
+		0x0, 0x0, 0x1c, 0x4a, // opaque
+		0x0, 0x0, 0x0, 0x0, 0x37, 0xef, 0x3a, 0x35, // CAS
+		1, 2, 3, 4, // extras
+		's', 'o', 'm', 'e', 'k', 'e', 'y',
+		's', 'o', 'm', 'e', 'v', 'a', 'l', 'u', 'e'}
+
+	if len(got) != req.Size() {
+		t.Fatalf("Expected %v bytes, got %v", got,
+			len(got))
+	}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Fatalf("Expected:\n%#v\n  -- got -- \n%#v",
+			expected, got)
+	}
+}
+
+func TestEncodingRequestWithLargeBody(t *testing.T) {
+	req := MCRequest{
+		Opcode:  SET,
+		Cas:     938424885,
+		Opaque:  7242,
+		VBucket: 824,
+		Extras:  []byte{1, 2, 3, 4},
+		Key:     []byte("somekey"),
+		Body:    make([]byte, 256),
+	}
+
+	buf := &bytes.Buffer{}
+	req.Transmit(buf)
+	got := buf.Bytes()
+
+	expected := append([]byte{
+		REQ_MAGIC, byte(SET),
+		0x0, 0x7, // length of key
+		0x4,       // extra length
+		0x0,       // reserved
+		0x3, 0x38, // vbucket
+		0x0, 0x0, 0x1, 0xb, // Length of remainder
+		0x0, 0x0, 0x1c, 0x4a, // opaque
+		0x0, 0x0, 0x0, 0x0, 0x37, 0xef, 0x3a, 0x35, // CAS
+		1, 2, 3, 4, // extras
+		's', 'o', 'm', 'e', 'k', 'e', 'y',
+	}, make([]byte, 256)...)
 
 	if len(got) != req.Size() {
 		t.Fatalf("Expected %v bytes, got %v", got,
